4a3aba1e75637d80a6038538305dbd2c77a31e7f
ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m <ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m>
2023-08-29 19:23:40 +0000

    changed . for -> to derreference a pointer

--- functions3.c
+++ functions3.c
@@ -11,12 +11,12 @@ void monty_swap(stack_t **stack, unsigned int line_num)
 {
 	(void) line_num;
 
-	stack_t *box = *stack.next;
+	stack_t *box = *stack->next;
 
-	*stack.prev = box;
-	*stack.next = box.next;
-	box.next = *stack;
-	box.prev = NULL;
+	*stack->prev = box;
+	*stack->next = box->next;
+	box->next = *stack;
+	box->prev = NULL;
 
 	*stack = box;
 }
@@ -33,15 +33,15 @@ void monty_add(stack_t **stack, unsigned int line_num)
 	(void) line_num;
 
 	stack_t *add;
-	stack_t *stackmod = (*stack.next).next;
+	stack_t *stackmod = (*stack->next)->next;
 
-	int first = *stack.n;
-	int second = (*stack.next).n;
+	int first = *stack->n;
+	int second = (*stack->next)->n;
 
-	add.prev = NULL;
-	add.n = (first) + (second);
-	add.next = stackmod;
-	stackmod.prev = add;
+	add->prev = NULL;
+	add->n = (first) + (second);
+	add->next = stackmod;
+	stackmod->prev = add;
 
 	free(first);
 	free(second);
